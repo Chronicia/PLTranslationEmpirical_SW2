1f15ec22399cac5c088e198efd1eb4fd85daddf0
khlnguyen <khlnguyen@connect.ust.hk>
2025-01-02 22:56:53 +0800

    add avatar benchmark script, refer to avatar_scripts.sh to the correct, replicable sequence. fix3 repair not runnable due to exceed openai token limit

--- avatar_scripts.md
+++ avatar_scripts.md
@@ -15,14 +15,41 @@ bash scripts/test_avatar.sh Java C gpt-4o-mini fix_reports 1
 bash scripts/test_avatar.sh Java Python gpt-4o-mini fix_reports 1
 ```
 
-Repairing Unsuccessful Translations
+Repairing Unsuccessful Translations (fix1 use "compile". Always run "Generate Report after repair" before continuing new fixes. Then fix2 use "runtime", fix3 use "incorrect")
 ```bash
-bash scripts/repair.sh gpt-4o-mini avatar C++ Java 50 0.95 0.7 0 1 compile {timestamp of the fix report}
-bash scripts/repair.sh gpt-4o-mini avatar Go Java 50 0.95 0.7 0 1 compile {timestamp of the fix report}
-bash scripts/repair.sh gpt-4o-mini avatar C Java 50 0.95 0.7 0 1 compile {timestamp of the fix report}
-bash scripts/repair.sh gpt-4o-mini avatar Python Java 50 0.95 0.7 0 1 compile {timestamp of the fix report}
+# Repair compile (fix1)
+bash scripts/repair.sh gpt-4o-mini avatar Java C++ 50 0.95 0.7 0 1 compile {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Go 50 0.95 0.7 0 1 compile {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java C 50 0.95 0.7 0 1 compile {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Python 50 0.95 0.7 0 1 compile {timestamp of the fix report}
+
+# Repair runtime (fix2)
+bash scripts/repair.sh gpt-4o-mini avatar Java C++ 50 0.95 0.7 0 2 runtime {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Go 50 0.95 0.7 0 2 runtime {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java C 50 0.95 0.7 0 2 runtime {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Python 50 0.95 0.7 0 2 runtime {timestamp of the fix report}
+
+# Repair incorrect (fix3)
+bash scripts/repair.sh gpt-4o-mini avatar Java C++ 50 0.95 0.7 0 3 incorrect {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Go 50 0.95 0.7 0 3 incorrect {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java C 50 0.95 0.7 0 3 incorrect {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Python 50 0.95 0.7 0 3 incorrect {timestamp of the fix report}
 ```
-  
+
+Generate Report after repair (Change "_IO_[n]" to the directory that contain new fixed files)
+```bash
+cd output && cp -r gpt-4o-mini_IO_[n]/avatar/Java/C++/* gpt-4o-mini/avatar/Java/C++ && cd ..
+bash scripts/test_avatar.sh Java C++ gpt-4o-mini fix_reports [n+1]
+
+cd output && cp -r gpt-4o-mini_IO_[n]/avatar/Java/Go/* gpt-4o-mini/avatar/Java/Go && cd ..
+bash scripts/test_avatar.sh Java Go gpt-4o-mini fix_reports [n+1]
+
+cd output && cp -r gpt-4o-mini_IO_[n]/avatar/Java/C/* gpt-4o-mini/avatar/Java/C && cd ..
+bash scripts/test_avatar.sh Java C gpt-4o-mini fix_reports [n+1]
+
+cd output && cp -r gpt-4o-mini_IO_[n]/avatar/Java/Python/* gpt-4o-mini/avatar/Java/Python && cd ..
+bash scripts/test_avatar.sh Java Python gpt-4o-mini fix_reports [n+1]
+```
 
 
 ## From Python
---
+++ avatar_scripts.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# Translation
+bash scripts/translate.sh gpt-4o-mini avatar Java C++ 50 0.95 0.7 0
+bash scripts/translate.sh gpt-4o-mini avatar Java Go 50 0.95 0.7 0
+bash scripts/translate.sh gpt-4o-mini avatar Java C 50 0.95 0.7 0
+bash scripts/translate.sh gpt-4o-mini avatar Java Python 50 0.95 0.7 0
+
+# Generate Report
+bash scripts/test_avatar.sh Java C++ gpt-4o-mini fix_reports 1
+bash scripts/test_avatar.sh Java Go gpt-4o-mini fix_reports 1
+bash scripts/test_avatar.sh Java C gpt-4o-mini fix_reports 1
+bash scripts/test_avatar.sh Java Python gpt-4o-mini fix_reports 1
+
+# Repair compile (fix1)
+bash scripts/repair.sh gpt-4o-mini avatar Java C++ 50 0.95 0.7 0 1 compile {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Go 50 0.95 0.7 0 1 compile {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java C 50 0.95 0.7 0 1 compile {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Python 50 0.95 0.7 0 1 compile {timestamp of the fix report}
+
+# Report fix1
+cd output && cp -r gpt-4o-mini_IO_1/avatar/Java/C++/* gpt-4o-mini/avatar/Java/C++ && cd ..
+bash scripts/test_avatar.sh Java C++ gpt-4o-mini fix_reports 2
+
+cd output && cp -r gpt-4o-mini_IO_1/avatar/Java/Go/* gpt-4o-mini/avatar/Java/Go && cd ..
+bash scripts/test_avatar.sh Java Go gpt-4o-mini fix_reports 2
+
+cd output && cp -r gpt-4o-mini_IO_1/avatar/Java/C/* gpt-4o-mini/avatar/Java/C && cd ..
+bash scripts/test_avatar.sh Java C gpt-4o-mini fix_reports 2
+
+cd output && cp -r gpt-4o-mini_IO_1/avatar/Java/Python/* gpt-4o-mini/avatar/Java/Python && cd ..
+bash scripts/test_avatar.sh Java Python gpt-4o-mini fix_reports 2
+
+# Repair runtime (fix2)
+bash scripts/repair.sh gpt-4o-mini avatar Java C++ 50 0.95 0.7 0 2 runtime {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Go 50 0.95 0.7 0 2 runtime {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java C 50 0.95 0.7 0 2 runtime {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Python 50 0.95 0.7 0 2 runtime {timestamp of the fix report}
+
+# Report fix2
+cd output && cp -r gpt-4o-mini_IO_2/avatar/Java/C++/* gpt-4o-mini/avatar/Java/C++ && cd ..
+bash scripts/test_avatar.sh Java C++ gpt-4o-mini fix_reports 3
+
+cd output && cp -r gpt-4o-mini_IO_2/avatar/Java/Go/* gpt-4o-mini/avatar/Java/Go && cd ..
+bash scripts/test_avatar.sh Java Go gpt-4o-mini fix_reports 3
+
+cd output && cp -r gpt-4o-mini_IO_2/avatar/Java/C/* gpt-4o-mini/avatar/Java/C && cd ..
+bash scripts/test_avatar.sh Java C gpt-4o-mini fix_reports 3
+
+cd output && cp -r gpt-4o-mini_IO_2/avatar/Java/Python/* gpt-4o-mini/avatar/Java/Python && cd ..
+bash scripts/test_avatar.sh Java Python gpt-4o-mini fix_reports 3
+
+# Repair incorrect (fix3)
+bash scripts/repair.sh gpt-4o-mini avatar Java C++ 50 0.95 0.7 0 3 incorrect 20241226_021007 {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Go 50 0.95 0.7 0 3 incorrect 20241226_021046 {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java C 50 0.95 0.7 0 3 incorrect 20241226_021241 {timestamp of the fix report}
+bash scripts/repair.sh gpt-4o-mini avatar Java Python 50 0.95 0.7 0 3 incorrect 20241226_021357 {timestamp of the fix report}
+
+# Report fix3
+cd output && cp -r gpt-4o-mini_IO_3/avatar/Java/C++/* gpt-4o-mini/avatar/Java/C++ && cd ..
+bash scripts/test_avatar.sh Java C++ gpt-4o-mini fix_reports 4
+
+cd output && cp -r gpt-4o-mini_IO_3/avatar/Java/Go/* gpt-4o-mini/avatar/Java/Go && cd ..
+bash scripts/test_avatar.sh Java Go gpt-4o-mini fix_reports 4
+
+cd output && cp -r gpt-4o-mini_IO_3/avatar/Java/C/* gpt-4o-mini/avatar/Java/C && cd ..
+bash scripts/test_avatar.sh Java C gpt-4o-mini fix_reports 4
+
+cd output && cp -r gpt-4o-mini_IO_3/avatar/Java/Python/* gpt-4o-mini/avatar/Java/Python && cd ..
+bash scripts/test_avatar.sh Java Python gpt-4o-mini fix_reports 4
